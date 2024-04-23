0ed24b9a51b7870d5b96aa1fe32d02d1433cf0d4
Mlook70 <Abdulmalek.akel@gmail.com>
2024-04-24 02:32:51 +0300

    Min Bio 0 char, Min Toky 2 char

--- lib/validation/toky.ts
+++ lib/validation/toky.ts
@@ -1,7 +1,7 @@
 import * as z from "zod";
 
 export const TokyValidation = z.object({
-  toky: z.string().nonempty().min(3, { message: "Minimum 3 characters." }),
+  toky: z.string().nonempty().min(2, { message: "Minimum 2 characters." }),
   accountId: z.string(),
 });
 
