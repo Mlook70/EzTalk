5409d084acd14d1e6bb5644207a7c5ffea9172b2
rifai <d666666dd@gmail.com>
2024-04-25 22:31:16 +0300

    ok

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,66 +1,91 @@
+import Image from "next/image";
 import { currentUser } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
 
+import { communityTabs } from "@/constants";
 
-import Pagination from "@/components/shared/Pagination";
-import CommunityCard from "@/components/cards/CommunityCard";
+import UserCard from "@/components/cards/UserCard";
+import TokiesTab from "@/components/shared/TokiesTab";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
+import { fetchCommunityDetails } from "@/lib/actions/community.actions";
 
-import { fetchCommunities } from "@/lib/actions/community.actions";
-import { fetchUser } from "@/lib/actions/user.action";
-import Searchbar from "@/components/shared/Srearchbar";
-
-async function Page({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | undefined };
-}) {
+async function Page({ params }: { params: { id: string } }) {
   const user = await currentUser();
   if (!user) return null;
 
-  const userInfo = await fetchUser(user.id);
-  if (!userInfo?.onboarded) redirect("/onboarding");
-
-  const result = await fetchCommunities({
-    searchString: searchParams.q,
-    pageNumber: searchParams?.page ? +searchParams.page : 1,
-    pageSize: 25,
-  });
+  const communityDetails = await fetchCommunityDetails(params.id);
 
   return (
-    <>
-      <h1 className='head-text'>Communities</h1>
-
-      <div className='mt-5'>
-        <Searchbar routeType='communities' />
-      </div>
-
-      <section className='mt-9 flex flex-wrap gap-4'>
-        {result.communities.length === 0 ? (
-          <p className='no-result'>No Result</p>
-        ) : (
-          <>
-            {result.communities.map((community) => (
-              <CommunityCard
-                key={community.id}
-                id={community.id}
-                name={community.name}
-                username={community.username}
-                imgUrl={community.image}
-                bio={community.bio}
-                members={community.members}
-              />
-            ))}
-          </>
-        )}
-      </section>
-
-      <Pagination
-        path='communities'
-        pageNumber={searchParams?.page ? +searchParams.page : 1}
-        isNext={result.isNext}
+    <section>
+      <ProfileHeader
+        accountId={communityDetails.createdBy.id}
+        authUserId={user.id}
+        name={communityDetails.name}
+        username={communityDetails.username}
+        imgUrl={communityDetails.image}
+        bio={communityDetails.bio}
+        type="Community"
       />
-    </>
+
+      <div className="mt-9">
+        <Tabs defaultValue="Tokies" className="w-full">
+          <TabsList className="tab">
+            {communityTabs.map((tab) => (
+              <TabsTrigger key={tab.label} value={tab.value} className="tab">
+                <Image
+                  src={tab.icon}
+                  alt={tab.label}
+                  width={24}
+                  height={24}
+                  className="object-contain"
+                />
+                <p className="max-sm:hidden">{tab.label}</p>
+
+                {tab.label === "Tokies" && (
+                  <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
+                    {communityDetails.threads.length}
+                  </p>
+                )}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+
+          <TabsContent value="Tokies" className="w-full text-light-1">
+            {/* @ts-ignore */}
+            <TokiesTab
+              currentUserId={user.id}
+              accountId={communityDetails._id}
+              accountType="Community"
+            />
+          </TabsContent>
+
+          <TabsContent value="members" className="mt-9 w-full text-light-1">
+            <section className="mt-9 flex flex-col gap-10">
+              {communityDetails.members.map((member: any) => (
+                <UserCard
+                  key={member.id}
+                  id={member.id}
+                  name={member.name}
+                  username={member.username}
+                  imgUrl={member.image}
+                  personType="User"
+                />
+              ))}
+            </section>
+          </TabsContent>
+
+          <TabsContent value="requests" className="w-full text-light-1">
+            {/* @ts-ignore */}
+            <TokiesTab
+              currentUserId={user.id}
+              accountId={communityDetails._id}
+              accountType="Community"
+            />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </section>
   );
 }
 
